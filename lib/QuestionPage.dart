c45bbaeb007c589bfde43d4617d4993c9f3b2c22
John Callahan <jcallahan@acm.org>
2017-12-13 14:01:37 -0500

    set grey color of other answers

--- lib/QuestionPage.dart
+++ lib/QuestionPage.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:quizcircle/QuestionListItem.dart';
 import 'package:quizcircle/model/Quiz.dart';
+import 'package:quizcircle/model/Answer.dart';
 import 'package:quizcircle/model/Question.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -21,6 +22,7 @@ class QuestionPage extends StatefulWidget {
 class QuestionPageState extends State<QuestionPage> {
 
   Question question;
+  List answers;
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = new GlobalKey<RefreshIndicatorState>();
   String _accessToken;
   String _url;
@@ -54,6 +56,10 @@ class QuestionPageState extends State<QuestionPage> {
     this.setState(() {
       Map map = JSON.decode(response.body);
       question = new Question(map["id"].toInt(),map["name"]);
+      answers = new List();
+      map["answers"].forEach((answer) {
+        answers.add(new Answer(answer["id"].toInt(),answer["name"],Colors.blue));
+      });
     });
   }
 
@@ -68,6 +74,19 @@ class QuestionPageState extends State<QuestionPage> {
   Widget build(BuildContext context) {
     if(question != null) {
       print("the question is ${question.name} with no answers");
+      List<Widget> answerButtons = new List<Widget>();
+      
+      int i = 0;
+      answers.forEach((answer) {
+        answerButtons.add(new Container(
+	  margin: const EdgeInsets.all(4.0),
+	  child: new RaisedButton(
+            child: new Text(answer.name, style: new TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22.0)),
+	    color: answer.color,
+	    onPressed: () { print(answer.name); setState(() { answers.forEach((a) {a.color = Colors.grey;}); answer.color = Colors.green; }); } )));
+	i = i + 1;
+      });
+
       return new Scaffold(
         appBar: new AppBar(
           title: new Text(widget.quiz.name),
@@ -77,8 +96,27 @@ class QuestionPageState extends State<QuestionPage> {
             child: new Column(
               mainAxisAlignment: MainAxisAlignment.center,
 	      children: <Widget>[
-	        new Text(question.name),
-		new Text("Unattempted questions are ${widget.quiz.unattempted}"),
+	        new Container(
+		  margin: const EdgeInsets.all(50.0),
+	          child: new Text(
+		    question.name,
+		    style: new TextStyle(color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 22.0),
+		  ),
+                ),
+		new Column(
+		  children: answerButtons,
+		),
+		new IconButton(
+		  icon: new Icon(Icons.favorite),
+		  tooltip: 'Start',
+		  iconSize: 70.0,
+		  onPressed: () {
+		    widget.quiz.unattempted.removeAt(0);
+		    Navigator.pushReplacement(context, new MaterialPageRoute(
+		      builder: (BuildContext context) => new QuestionPage(widget.quiz),
+		    ));
+		  }
+		),
               ]
 	    )
 	  )
---
+++ lib/model/Answer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class Answer {
+  int id;
+  String name;
+  MaterialColor color;
+
+  Answer(this.id, this.name, this.color);
+}
