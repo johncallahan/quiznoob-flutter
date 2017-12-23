331b0f532d3ec7601539f12247443eea04408a6e
John Callahan <jcallahan@acm.org>
2017-12-23 09:33:51 -0500

    oops, forgot to add splash screen files

---
+++ lib/Splash.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:collection';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:quizcircle/Subjects.dart';
+import 'package:quizcircle/AppSettings.dart';
+import 'package:quizcircle/model/User.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => new _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  String _accessToken = null;
+  String _url = null;
+  User _user = null;
+
+  @override
+  void initState() async {
+    await this._getSharedPreferences();
+    if(_url != null && _accessToken != null) {
+      this.getData();
+    }
+  }
+
+  _getSharedPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    this.setState(() {
+      _url = prefs.getString("url");
+      _accessToken = prefs.getString("token");
+    });
+  }
+
+  Future<Null> getData() async {
+    http.Response response = await http.post(
+      Uri.encodeFull("${_url}/api/users.json"),
+        body: {"access_token": _accessToken},
+        headers: {
+          "Accept":"application/json"
+	}
+      );
+      this.setState(() {
+         Map map = JSON.decode(response.body);
+	 _user = new User(map["id"].toInt(), map["name"], map["hearts"].toInt());
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(_user != null) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Welcome!"),
+	  backgroundColor: Colors.green,
+        ),
+        body: new Container(
+          child: new Center(
+            child: new Column(
+	      mainAxisAlignment: MainAxisAlignment.center,
+	      children: <Widget>[
+	        new IconButton(
+	          icon: new Icon(Icons.favorite, color: Colors.red),
+		  iconSize: 70.0,
+		  onPressed: () {
+                    Navigator.pushReplacement(context, new MaterialPageRoute(
+		      builder: (BuildContext context) => new Subjects(),
+		    ));
+		  }
+                ),
+	        new Text("Welcome ${_user.name}!"),
+	      ]
+            )
+          )
+        )
+      );
+    } else {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Welcome!"),
+	  backgroundColor: Colors.green,
+        ),
+        body: new Container(
+          child: new Center(
+            child: new Column(
+	      mainAxisAlignment: MainAxisAlignment.center,
+	      children: <Widget>[
+	        new IconButton(
+	          icon: new Icon(Icons.favorite, color: Colors.red),
+		  iconSize: 70.0,
+		  onPressed: () {
+                    Navigator.pushReplacement(context, new MaterialPageRoute(
+		      builder: (BuildContext context) => new AppSettings(),
+		    ));
+		  }
+                ),
+	        new Text("Welcome!"),
+	      ]
+            )
+          )
+        )
+      );
+    }
+  }
+}
---
+++ lib/model/User.dart
@@ -0,0 +1,7 @@
+class User {
+  int id;
+  String name;
+  int hearts;
+
+  User(this.id, this.name, this.hearts);
+}
